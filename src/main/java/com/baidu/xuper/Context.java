e51c0038f19eb5988ccfa3e3148e9982ae610865
chenfengjin <1871653365@qq.com>
2021-09-02 10:34:55 +0800

    Add context documentation

--- src/main/java/com/baidu/xuper/Context.java
+++ src/main/java/com/baidu/xuper/Context.java
@@ -20,15 +20,14 @@ public interface Context {
     public List<String> authRequire();
 
     // putObject put a object into state, all the value is  transaction isolated,you can read the KV pair
-    // in the same transaction, you can visit it through NewIterator,
+    // in the same transaction, you can visit it through newIterator,
     // but you can not read it in other transaction until the transaction is commited
-    // error may be no nil, a typical case is networking error
     public void putObject(byte[] key, byte[] value);
 
-    // getObject return a object, see PutObject for more details
+    // getObject return a object, see putObject for more details
     public byte[] getObject(byte[] key);
 
-    // deleteObject delete a object, see PutObject for more details
+    // deleteObject delete a object, see putObject for more details
     public void deleteObject(byte[] key);
 
     // newIterator return a range iterator
@@ -44,7 +43,7 @@ public interface Context {
     // transfer transfer an amount fo a address
     public void transfer(String to, BigInteger amount);
 
-    // transferAmount get transfer amount of an ContractCall
+    // transferAmount get transfer amount of an contract call
     public BigInteger transferAmount();
 
     // call start a in-contract call,module can be wasm|native|evm, dependding on contract you call
@@ -53,12 +52,12 @@ public interface Context {
     // crossQuery start a cross chain query, see https://xuper.baidu.com/n/xuperdoc/ CrossQuery section for more information.
     public Response crossQuery(String uri, Map<String, byte[]> args);
     
-    // EmitEvent emit a event with name
+    // emitEvent emit a event with name
     public void emitEvent(String name, byte[] body);
 
-    //  EmmitJSONEvent emit an event, you can use any JSON marshalable body,  body will be marshaled using GSON
+    //  emmitJSONEvent emit an event, body will be marshaled using GSON
     public void emitJSONEvent(String name, Object body);
 
-    // Log emmit a log entry, see fmt.Sprintf for log format
+    // log emmit a log entry
     public void log(String msg);
 }
